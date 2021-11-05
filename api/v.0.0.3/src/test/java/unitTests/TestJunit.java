e48c0bb7fd5faa6cd08907a9fb30812c88509f43
Denys Shabelnyk <dionisiy1986@gmail.com>
2021-11-05 23:36:50 +0200

    regular daily commit

---
+++ api/v.0.0.3/src/test/java/unitTests/TestJunit.java
@@ -0,0 +1,23 @@
+//ready to commit v0.0.3
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestJunit {
+
+	@Test
+	public void testAdd() {
+		String str = "First try";
+		assertEquals("First try", str);
+	}
+
+	// need test
+	// TODO: check if dataSource bean created
+	public void checkDbConnection() {
+
+	}
+	// ... //
+}
+// ... //
