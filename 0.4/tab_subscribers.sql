9eedaa451fe0ffe7c848498f4dc13f4e7d2c2305
Denys Shabelnyk <dionisiy1986@gmail.com>
2021-11-11 22:40:10 +0200

    regular daily commit

---
+++ 0.4/tab_subscribers.sql
@@ -0,0 +1,22 @@
+CREATE TABLE auth.subscribers (
+	id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+	login varchar NOT NULL,
+	"password" varchar NOT NULL,
+	firstname varchar NULL,
+	lastname varchar NULL,
+	"type" char NOT NULL DEFAULT 'Y',
+	lastlogindate timestamp NULL,
+	CONSTRAINT subscribers_pk PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX subscribers_login_idx ON auth.subscribers (login);
+COMMENT ON TABLE auth.subscribers IS 'Table of users';
+
+-- Column comments
+
+COMMENT ON COLUMN auth.subscribers.id IS 'UserID in the table';
+COMMENT ON COLUMN auth.subscribers.login IS 'login of subscribers';
+COMMENT ON COLUMN auth.subscribers."password" IS 'password of subscribers';
+COMMENT ON COLUMN auth.subscribers.firstname IS 'firstname of subscribers';
+COMMENT ON COLUMN auth.subscribers.lastname IS 'lastname of subscribers';
+COMMENT ON COLUMN auth.subscribers."type" IS 'permission by default';
+COMMENT ON COLUMN auth.subscribers.lastlogindate IS 'last succesfully authorization';
