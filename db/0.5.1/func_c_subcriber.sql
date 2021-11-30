668e14448d4e7e53e36596c0d19f19f880b4c2ff
Denys Shabelnyk <dionisiy1986@gmail.com>
2021-11-30 23:07:54 +0200

    v0.5 and v0.5.1

---
+++ db/0.5.1/func_c_subcriber.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE FUNCTION auth.c_subscriber(i_name character varying, i_surname character varying, i_login character varying, i_password character varying)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+declare 
+v_result int4;
+v_count int4;
+begin
+	
+	-- if login already exists - return -1 --
+	select count(*) 
+	   into v_count
+	from auth.subscribers s
+	where s.login = i_login;
+
+    if v_count > 0 then
+       v_result = -1;
+      return v_result;
+    end if;
+    
+	-- return values: 0 - success, -1 - error
+	insert into auth.subscribers(id,login,password,firstname,lastname,lastlogindate)
+		values (nextval('auth.seq_subscribers_id'),i_login,i_password,i_name,i_surname,current_timestamp);
+    
+	-- check wheater of registrarion --
+   select count(*) 
+	   into v_count
+	from auth.subscribers s
+	where s.login = i_login;
+
+  if v_count > 0 then 
+     v_result = 0;
+  else
+     v_result = -1;
+  end if;
+
+  return v_result;
+     end 
+$function$;
---
+++ db/0.5.1/tab_subscribersmodes.sql
@@ -0,0 +1,28 @@
+CREATE TABLE auth.subscribersmodes (
+	id bigint NOT NULL,
+	subid bigint NOT NULL,
+	login int8 NULL DEFAULT 0,
+	"password" int8 NULL DEFAULT 0,
+	firstname int8 NULL DEFAULT 0,
+	lastname int8 NULL DEFAULT 0,
+	loginlasttime timestamp NULL,
+	passwordlasttime timestamp NULL,
+	fnamelasttime timestamp NULL,
+	lnamelasttime timestamp NULL,
+	CONSTRAINT subscribersmodes_pk PRIMARY KEY (id),
+	CONSTRAINT subscribersmodes_fk FOREIGN KEY (subid) REFERENCES auth.subscribers(id) ON DELETE CASCADE
+);
+COMMENT ON TABLE auth.subscribersmodes IS 'Table for save changing in subscribers table';
+
+-- Column comments
+
+COMMENT ON COLUMN auth.subscribersmodes.id IS 'primary key';
+COMMENT ON COLUMN auth.subscribersmodes.subid IS 'foreign key of subscribers table';
+COMMENT ON COLUMN auth.subscribersmodes.login IS 'counter of changing login';
+COMMENT ON COLUMN auth.subscribersmodes."password" IS 'counter of changing password';
+COMMENT ON COLUMN auth.subscribersmodes.firstname IS 'counter of change firstname';
+COMMENT ON COLUMN auth.subscribersmodes.lastname IS 'counter of change lastname';
+COMMENT ON COLUMN auth.subscribersmodes.loginlasttime IS 'time mark for last login change';
+COMMENT ON COLUMN auth.subscribersmodes.passwordlasttime IS 'time mark of last change password';
+COMMENT ON COLUMN auth.subscribersmodes.fnamelasttime IS 'time mark of last changing firstname';
+COMMENT ON COLUMN auth.subscribersmodes.lnamelasttime IS 'time mark of last changing lastname';
