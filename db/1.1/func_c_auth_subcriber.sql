f90c1edde6f0fb287611e4978c577f34452ccd1d
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-01-11 16:40:06 +0200

    v1.1 final to main

---
+++ db/1.1/func_c_auth_subcriber.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE FUNCTION auth.c_auth_subcriber(i_login character varying, i_password character varying)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+declare 
+v_login varchar;
+v_password varchar;
+v_result int4;
+begin
+	
+	-- if input parameters are empty --
+	if ((i_login is null) or (i_password is null)) 
+	   or (i_login is null and i_password is null) then 
+	v_result = -1;
+    return v_result;
+    end if;
+	
+	
+	select login,password 
+	   into v_login,v_password
+	from auth.subscribers s
+	where s.login = i_login
+	and s.password = i_password;
+
+   -- find subscriber --  
+if (v_login = i_login) and (v_password = i_password) then 
+     v_result = 0;
+  else
+     v_result = -1;
+  end if;
+
+  return v_result;
+     end 
+$function$
+;
