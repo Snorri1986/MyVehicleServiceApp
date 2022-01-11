41915394db5de64036dfbeaf591f7307eac7f8a8
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-01-11 17:03:29 +0200

    0.5 doc to main

---
+++ documentation/web/0.5/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"snorri1986.main","l":"MainPageController"},{"p":"models","l":"SubscriberAuthModelRequest"},{"p":"models","l":"SubscriberModel"}];updateSearchResults();
