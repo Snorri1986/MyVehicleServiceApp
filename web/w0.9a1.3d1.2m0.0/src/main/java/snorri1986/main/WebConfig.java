98e40fc9896fa748fc3b890d39c4febe3b36b7ec
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-03-30 20:23:05 +0300

    w0.9a1.3d1.2m0.0
    1.package snorri1986.main -> public class WebConfig:
    1) added bean localeChangeInterceptor() as Java method;
    2) added bean localeResolver() as Java method;
    3) added bean configurer()

--- web/w0.9a1.3d1.2m0.0/src/main/java/snorri1986/main/WebConfig.java
+++ web/w0.9a1.3d1.2m0.0/src/main/java/snorri1986/main/WebConfig.java
@@ -1,12 +1,19 @@
 package snorri1986.main;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -30,4 +37,37 @@ public class WebConfig implements WebMvcConfigurer {
 
 		return bean;
 	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver r = new CookieLocaleResolver();
+		r.setDefaultLocale(Locale.US);
+		r.setCookieName("localeInfo");
+
+		// if set to -1, the cookie is deleted
+		// when browser shuts down
+		r.setCookieMaxAge(24 * 60 * 60);
+		return r;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Bean
+	public WebMvcConfigurer configurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				LocaleChangeInterceptor l = new LocaleChangeInterceptor();
+				l.setParamName("lang");
+				registry.addInterceptor(l);
+			}
+		};
+	}
+
 }
