790a5a3e5c26d90bf7c13e08b486498271de1c12
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-05-31 22:52:31 +0300

    w0.12a1.6d1.4m0.0
    Task #61
    1.package main -> class WsMvsVehicleApplication:
    1) was add new variables with @Value annotation: String dbHerokuUri, String unHeroku, String passHeroku;
    2) was add new annotations under the class -> @Configuration and @EnableAutoConfiguration;
    3) was add getters and setters for variables;
    4) was add new @Bean PoolProperties and BasicDataSource, getJpaProperties() and LocalContainerEntityManagerFactoryBean.
    2.application.properties:
    1) was add parameters which depended with DataSource connection pool, Spring Jackson and PosgreSQL configuration.
    3. package unitTests -> class TestJunit:
    1) was add new package with test cases.

--- api/ws-mvs-vehicle/w0.12a1.6d1.4m0.0/src/main/java/main/WsMvsVehicleApplication.java
+++ api/ws-mvs-vehicle/w0.12a1.6d1.4m0.0/src/main/java/main/WsMvsVehicleApplication.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 /**
  * Description: The main class. Extends SpringBootServletInitializer
@@ -13,9 +29,122 @@ import org.springframework.context.ApplicationContext;
  * @since w0.12a1.6d1.4m0.0
  */
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class })
 public class WsMvsVehicleApplication extends SpringBootServletInitializer {
 
 	private static ApplicationContext context;
+
+	@Value("${spring.datasource.uri}")
+	String dbHerokuUri;
+
+	@Value("${spring.datasource.username}")
+	String unHeroku;
+
+	@Value("${spring.datasource.password}")
+	String passHeroku;
+
+	/**
+	 * Description: Getter for username to Heroku Cloud
+	 *
+	 * @since w0.12a1.6d1.4m0.0
+	 * @return String with username
+	 */
+	public String getUnHeroku() {
+		return unHeroku;
+	}
+
+	/**
+	 * Description: Getter for password to Heroku Cloud
+	 *
+	 * @since w0.12a1.6d1.4m0.0
+	 * @return String with password
+	 */
+	public String getPassHeroku() {
+		return passHeroku;
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.datasource.tomcat")
+	/**
+	 * Description: Bean.DataSource pool connections
+	 *
+	 * @since w0.12a1.6d1.4m0.0
+	 * @return PoolProperties instance
+	 */
+	public PoolProperties getDataSourcePoolProperties() {
+		return new PoolProperties();
+	}
+
+	@Bean
+	/**
+	 * Description: Bean. DataSource PostgreSQL
+	 *
+	 * @since w0.12a1.6d1.4m0.0
+	 * @return BasicDataSource instance
+	 * @throws URISyntaxException - connection string
+	 */
+	public BasicDataSource dataSource() throws URISyntaxException {
+		URI dbUri = new URI(dbHerokuUri);
+
+		String username = unHeroku;
+		String password = passHeroku;
+
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
+	}
+
+	/**
+	 * Description: getter method of JPA properties(Bean)
+	 *
+	 * @author Denys Shabelnyk
+	 * @since w0.12a1.6d1.4m0.0
+	 * @return Properties - return current JPA properties
+	 */
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.jpa")
+	public Properties getJpaProperties() {
+		return new Properties();
+	}
+
+	/**
+	 * Description: factory to EntityManager
+	 *
+	 * @author Denys Shabelnyk
+	 * @since w0.12a1.6d1.4m0.0
+	 * @return LocalContainerEntityManagerFactoryBean - return current instance
+	 * @throws ClassNotFoundException - if a class didn't find
+	 */
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactory() throws ClassNotFoundException {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		Properties jpaProperties = getJpaProperties();
+		vendorAdapter.setGenerateDdl(false);
+		final boolean showsql = Boolean.parseBoolean(jpaProperties.getProperty("show-sql", "false"));
+		vendorAdapter.setShowSql(showsql);
+
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		try {
+			factoryBean.setDataSource(dataSource());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		factoryBean.setPackagesToScan("repository");
+		factoryBean.setJpaProperties(jpaProperties);
+
+		return factoryBean;
+	}
 
 	@Override
 	/**
