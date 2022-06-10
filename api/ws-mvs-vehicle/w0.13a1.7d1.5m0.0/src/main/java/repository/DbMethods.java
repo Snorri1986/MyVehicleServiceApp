a9babda7b03d7c8c410177b933be14749024b17d
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-06-10 21:40:37 +0300

    w0.13a1.7d1.5m0.0
    Task #68 part 3.
    1.Created new package "repository";
    2.In the "repository" package created new class -> DbMethods with @Repository annotation;
    3. In the DbMethods class defined method -> public Integer addNewVehicle(String login, String brand, String model, Integer yearofmanufacturing,
    			Integer totalmileage, Date lastservicedate, String type, String statenumber)
    which will save vehicle information from web part and save it in the database table.

---
+++ api/ws-mvs-vehicle/w0.13a1.7d1.5m0.0/src/main/java/repository/DbMethods.java
@@ -0,0 +1,66 @@
+package repository;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Description: The repository class
+ *
+ * @author Denys Shabelnyk
+ * @since w0.13a1.7d1.5m0.0
+ */
+@Repository
+public class DbMethods {
+
+	@Autowired
+	EntityManager entityManager;
+
+	/**
+	 * Description: method for adding new vehicle
+	 *
+	 * @author Denys Shabelnyk
+	 * @since w0.13a1.7d1.5m0.0
+	 * @param login               - subscriber's login
+	 * @param brand               - brand of a vehicle
+	 * @param model               - model of a vehicle
+	 * @param yearofmanufacturing - year of manufacturing
+	 * @param totalmileage        - total milleage of the vehicle
+	 * @param lastservicedate     - last service check of the vehicle
+	 * @param type                - type of a vehicle
+	 * @param statenumber         - state number of a vehicle
+	 * @return Integer - result code. 0 - success, -1 - fail
+	 */
+	public Integer addNewVehicle(String login, String brand, String model, Integer yearofmanufacturing,
+			Integer totalmileage, Date lastservicedate, String type, String statenumber) {
+		Integer answerCode = 0;
+
+		StoredProcedureQuery query = entityManager.createStoredProcedureQuery("park.c_addnewvehicle")
+				.registerStoredProcedureParameter("i_usrname", String.class, ParameterMode.IN)
+				.setParameter("i_usrname", login)
+				.registerStoredProcedureParameter("i_brand", String.class, ParameterMode.IN)
+				.setParameter("i_brand", brand)
+				.registerStoredProcedureParameter("i_model", String.class, ParameterMode.IN)
+				.setParameter("i_model", model)
+				.registerStoredProcedureParameter("i_dmanuf", Integer.class, ParameterMode.IN)
+				.setParameter("i_dmanuf", yearofmanufacturing)
+				.registerStoredProcedureParameter("i_tmilleage", Integer.class, ParameterMode.IN)
+				.setParameter("i_tmilleage", totalmileage)
+				.registerStoredProcedureParameter("i_lastservice", Date.class, ParameterMode.IN)
+				.setParameter("i_lastservice", lastservicedate)
+				.registerStoredProcedureParameter("i_type", String.class, ParameterMode.IN).setParameter("i_type", type)
+				.registerStoredProcedureParameter("i_statenumber", String.class, ParameterMode.IN)
+				.setParameter("i_statenumber", statenumber)
+				.registerStoredProcedureParameter("v_result", Integer.class, ParameterMode.OUT);
+
+		query.execute();
+		answerCode = (Integer) query.getOutputParameterValue("v_result");
+		return answerCode;
+	}
+
+}
