66eaf579949857181d526a05665af50763a27a93
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-06-10 21:37:56 +0300

    w0.13a1.7d1.5m0.0
    Task #68. Update documentation after task 3

--- api/ws-mvs-vehicle/documentation/package-search-index.js
+++ api/ws-mvs-vehicle/documentation/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"models"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"models"},{"l":"repository"}];updateSearchResults();
