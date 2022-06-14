4e95c383d768a0d74e019ab9d5e93ec07ac913ca
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-06-14 21:11:06 +0300

    w0.13a1.7d1.5m0.0
    update documention after task #68

--- api/ws-mvs-vehicle/documentation/type-search-index.js
+++ api/ws-mvs-vehicle/documentation/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"models","l":"AddNewVehicleRequest"},{"p":"models","l":"AddNewVehicleResponse"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"repository","l":"DbMethods"},{"p":"main","l":"WsMvsVehicleApplication"}];updateSearchResults();
+typeSearchIndex = [{"p":"controllers","l":"AddNewVehicleController"},{"p":"models","l":"AddNewVehicleRequest"},{"p":"models","l":"AddNewVehicleResponse"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"repository","l":"DbMethods"},{"p":"main","l":"WsMvsVehicleApplication"}];updateSearchResults();
